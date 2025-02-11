2e42dc96a3fbcf4e4715b18a07e2e1d57dbdc7c4
cyberfoxIT <58806167+cyberfoxIT@users.noreply.github.com>
2025-02-11 17:42:13 +0100

    Add files via upload

--- tcp_listener.js
+++ tcp_listener.js
@@ -2,17 +2,19 @@ const net = require('net');
 const path = require('path');
 const fsp = require('fs').promises;
 const fs = require('fs');
+
 const args = process.argv;
 
 let PORT = 3000;
 
-if (args.length > 2) {
+if (args.length > 2)
     PORT = args[2];
-}
 
 Number.prototype.padLeft = function(base,chr){
+
     var  len = (String(base || 10).length - String(this).length)+1;
     return len > 0? new Array(len).join(chr || '0')+this : this;
+
 }
 
 function padNumberWithSpaces(number, totalLength) {
@@ -22,16 +24,35 @@ function padNumberWithSpaces(number, totalLength) {
     let paddedNumber = numberString.padStart(totalLength, ' ');
     
     return paddedNumber;
+
 }
 
 function getIPv4MappedAddress(address) {
-    if (address.startsWith('::ffff:')) {
+
+    if (address.startsWith('::ffff:'))
         return address.replace('::ffff:', '');
-    }
+
     return address;
+
+}
+
+function reverseString(string){
+
+	var result = "";
+
+	for (var idx = 0; idx < string.length; idx++){
+		if (string.charAt(idx).toLowerCase() == string.charAt(idx))
+			result += string.charAt(idx).toUpperCase();
+		else
+			result += string.charAt(idx).toLowerCase();
+	}
+	
+	return result;
+
 }
 
 async function readDirectoryAndSend(directoryPath, socket) {
+
     var d = new Date,
         now = [(d.getMonth() + 1).toString().padStart(2, '0'),
                d.getDate().toString().padStart(2, '0'),
@@ -39,6 +60,7 @@ async function readDirectoryAndSend(directoryPath, socket) {
               [d.getHours().toString().padStart(2, '0'),
                d.getMinutes().toString().padStart(2, '0'),
                d.getSeconds().toString().padStart(2, '0')].join(':');
+
     try {
         const resolvedPath = path.resolve(directoryPath);
         const entries = await fsp.readdir(resolvedPath, { withFileTypes: true });
@@ -54,88 +76,102 @@ async function readDirectoryAndSend(directoryPath, socket) {
         for (const entry of sortedEntries) {
             const entryPath = path.join(resolvedPath, entry.name);
             if (entry.isFile()) {
-                socket.write(`      ${entryPath}\r`.replace(/\\/g, '/').toLowerCase());
-            } else if (entry.isDirectory()) {
-                socket.write(`<DIR> ${entryPath}\r`.replace(/\\/g, '/').toLowerCase());
-            }
+				try{
+					const stats = await fsp.stat(entryPath);
+					const fileSize = stats.size;
+					
+					socket.write(reverseString(`${fileSize.toString().padStart(10, ' ')} ${entryPath}\r`.replace(/\\/g, '/')));
+				} catch (err){
+				}
+            } else if (entry.isDirectory())
+				socket.write(reverseString(`     <DIR> ${entryPath}\r`.replace(/\\/g, '/')));
         }
         console.log(now + ' ' + getIPv4MappedAddress(socket.remoteAddress) + '                want to see dir ' + path.resolve(directoryPath));
     } catch (err) {
         console.log(now + ' ' + getIPv4MappedAddress(socket.remoteAddress) + ' ERR            want to see dir ' + directoryPath);
     }
+
 }
-
 const server = net.createServer((socket) => {
-    
+	
 	const clientAddress = getIPv4MappedAddress(socket.remoteAddress);
-	socket.setEncoding('utf8');
-
-	let fileName = '';
-	socket.on('data', (data) => {
-		const receivedMessage = data.toString();
-		if (receivedMessage.toUpperCase().startsWith("GET")){
-			try{
-				var d = new Date,
-					now = [(d.getMonth()+1).padLeft(),
-						  d.getDate().padLeft(),
-						  d.getFullYear()].join('/') +' ' +
-						  [d.getHours().padLeft(),
-						  d.getMinutes().padLeft(),
-						  d.getSeconds().padLeft()].join(':');
-			   
+	
+	var fileName = '';
+	var isPut = false;	
+	var data = [];
+	var recdatalen = 0;
+	
+	socket.on('data', function(chunk) {
+		if (isPut){
+			data.push(Buffer.from(chunk));
+			recdatalen += Buffer.from(chunk).length;
+		} else {
+			const receivedMessage = chunk.toString();
+			if (receivedMessage.toUpperCase().startsWith("GET") || receivedMessage.toUpperCase().startsWith("DXX")){
+				try{
+					var d = new Date,
+						now = [(d.getMonth()+1).padLeft(),
+							d.getDate().padLeft(),
+							d.getFullYear()].join('/') +' ' +
+							[d.getHours().padLeft(),
+							d.getMinutes().padLeft(),
+							d.getSeconds().padLeft()].join(':');
+				
+					fileName = receivedMessage.substring(4).replace(/\//g, '\\');
+					fs.readFile(fileName, (err, fileContent) => {
+						if (err) {
+							console.log(now + ' ' + clientAddress + ' ERR            want to get file ' + fileName);
+						} else {
+							socket.write(fileContent, () => {
+								console.log(now + ' ' + clientAddress + '     ' + padNumberWithSpaces(fileContent.length, 10) + ' want to get file ' + fileName);
+							});				
+						}
+					});
+				} catch (error){
+					console.log(now + ' ' + clientAddress + ' ERR            want to get file ' + fileName);
+				}
+			} else if (receivedMessage.toUpperCase().startsWith("DIR")){
+				try{
+					var d = new Date,
+						now = [(d.getMonth()+1).padLeft(),
+							d.getDate().padLeft(),
+							d.getFullYear()].join('/') +' ' +
+							[d.getHours().padLeft(),
+							d.getMinutes().padLeft(),
+							d.getSeconds().padLeft()].join(':');
+				
+					dirPath = receivedMessage.substring(4).replace(/\//g, '\\');
+					readDirectoryAndSend(dirPath, socket);
+				} catch (error){
+					console.log(now + ' ' + clientAddress + ' ERR            want to see dir  ' + path.resolve(dirPath));
+				}				
+			} else if (receivedMessage.toUpperCase().startsWith("PUT")){
 				fileName = receivedMessage.substring(4).replace(/\//g, '\\');
-				fs.readFile(fileName, (err, fileContent) => {
-					if (err) {
-						console.log(now + ' ' + clientAddress + ' ERR            want to get file ' + fileName);
-					} else {
-						socket.write(fileContent, () => {
-							console.log(now + ' ' + clientAddress + '     ' + padNumberWithSpaces(fileContent.length, 10) + ' want to get file ' + fileName);
-						});				
+				try{
+					if (fs.existsSync(fileName)) {
+						fs.unlinkSync(fileName);
 					}
-				});
-			} catch (error){
-				console.log(now + ' ' + clientAddress + ' ERR            want to get file ' + fileName);
+					isPut = true;
+				} catch (error){
+					console.log(now + ' ' + clientAddress + ' ERR            want to put file ' + fileName);
+				}
 			}
-		} else if (receivedMessage.toUpperCase().startsWith("DIR")){
-			try{
-				var d = new Date,
-					now = [(d.getMonth()+1).padLeft(),
-						  d.getDate().padLeft(),
-						  d.getFullYear()].join('/') +' ' +
-						  [d.getHours().padLeft(),
-						  d.getMinutes().padLeft(),
-						  d.getSeconds().padLeft()].join(':');
-			   
-				dirPath = receivedMessage.substring(4).replace(/\//g, '\\');
-				readDirectoryAndSend(dirPath, socket);
-			} catch (error){
-				console.error(error);
-				console.log(now + ' ' + clientAddress + ' ERR            want to see dir  ' + path.resolve(dirPath));
-			}				
-		} else if (receivedMessage.toUpperCase().startsWith("PUT")){
-			var d = new Date,
+		}
+	}).on('end', () => {
+		var d = new Date,
 				now = [(d.getMonth()+1).padLeft(),
 					  d.getDate().padLeft(),
 					  d.getFullYear()].join('/') +' ' +
 					  [d.getHours().padLeft(),
 					  d.getMinutes().padLeft(),
 					  d.getSeconds().padLeft()].join(':');
-		   
-			fileName = receivedMessage.substring(4).replace(/\//g, '\\');
-			isPut = true;
-		} else if (isPut){
-			fsp.appendFile(fileName, data, (err) => {
-				if (err) {
-					console.log(now + ' ' + clientAddress + ' ERR            want to put file ' + fileName);
-				} else {
-					console.log(now + ' ' + clientAddress + ' ' + padNumberWithSpaces(data.length, 10) + ' want to put file ' + fileName);
-					socket.read(data);
-				}
-			});
-		}
-			
-	});
-
+        if (isPut) {
+			fsp.writeFile(fileName, data); 
+			console.log(now + ' ' + clientAddress + '     ' + padNumberWithSpaces(recdatalen, 10) + ' want to put file ' + fileName);
+        }
+		
+    });
+	
 });
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
